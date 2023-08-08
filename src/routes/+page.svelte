6223c6db09c67ae43d75a4217dca028c9ccfec84
Sebastian <swebermorales@gmail.com>
2023-08-08 15:55:16 -0700

    adding more theme colors

---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<div class="container px-20 py-3 flex flex-col gap-4">
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Remember me</span>
+			<input type="checkbox" checked class="checkbox" />
+		</label>
+	</div>
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Remember me</span>
+			<input type="checkbox" checked class="checkbox checkbox-primary" />
+		</label>
+	</div>
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Remember me</span>
+			<input type="checkbox" checked class="checkbox checkbox-secondary" />
+		</label>
+	</div>
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Remember me</span>
+			<input type="checkbox" checked class="checkbox checkbox-accent" />
+		</label>
+	</div>
+	<div class="form-control">
+		<label class="label cursor-pointer">
+			<span class="label-text">Remember me</span>
+			<input type="checkbox" checked class="checkbox checkbox-success" />
+		</label>
+	</div>
+
+	<div class="flex flex-col gap-5">
+		<div class="collapse collapse-arrow bg-base-200">
+			<input type="radio" name="my-accordion-2" checked />
+			<div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+			<div class="collapse-content">
+				<p>hello</p>
+			</div>
+		</div>
+		<div class="collapse collapse-arrow bg-base-200">
+			<input type="radio" name="my-accordion-2" />
+			<div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+			<div class="collapse-content">
+				<p>hello</p>
+			</div>
+		</div>
+		<div class="collapse collapse-arrow bg-base-200">
+			<input type="radio" name="my-accordion-2" />
+			<div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+			<div class="collapse-content">
+				<p>hello</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="badge">default</div>
+	<div class="badge badge-neutral">neutral</div>
+	<div class="badge badge-primary">primary</div>
+	<div class="badge badge-secondary">secondary</div>
+	<div class="badge badge-accent">accent</div>
+	<div class="badge badge-ghost">ghost</div>
+
+	<div class="flex flex-col w-full">
+		<div class="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+		<div class="divider" />
+		<div class="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+	</div>
+
+	<progress class="progress w-56" value="0" max="100" />
+	<progress class="progress w-56" value="10" max="100" />
+	<progress class="progress w-56" value="40" max="100" />
+	<progress class="progress w-56" value="70" max="100" />
+	<progress class="progress w-56" value="100" max="100" />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,14 @@ export default {
 					primary: '#FF550D',
 					'primary-focus': '#FF7438',
 					'base-100': '#101828',
-					'base-200': '#1D2939'
+					'base-200': '#1D2939',
+					'base-300': '#344054',
+					'base-content': '#ffff',
+					'neutral-content': '#E8E8E8',
+					neutral: '#2a323c',
+					accent: '#026AA2',
+					success: '#039855',
+					secondary: '#FE7B43'
 				},
 				light: {
 					...require('daisyui/src/theming/themes')['[data-theme=light]']
