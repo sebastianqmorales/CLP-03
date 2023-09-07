bd1ad291e5dde1f85e2788904c1e04856bd2ecb2
Sebastian <swebermorales@gmail.com>
2023-09-07 15:48:03 -0700

    inital commit, basic post functionality

--- src/routes/auth/admin/+page.svelte
+++ src/routes/auth/admin/+page.svelte
@@ -2,3 +2,16 @@
 <form action="./?/logout" method="POST">
 	<button type="submit" class="btn btn-primary">Logout</button>
 </form>
+
+<!-- 
+	form
+		inputs, title
+		inputs, excerpt
+		tiptap editor needs to be equal to body.content
+ -->
+<form action="?/submitBlog" method="POST" class="form-control gap-4 px-20">
+	<input class="input input-bordered w-[300px]" placeholder="Title" type="text" name="title" />
+	<input class="input input-bordered w-[300px]" placeholder="Excerpt" type="text" name="excerpt" />
+	<input class="input input-bordered w-[300px]" placeholder="Content" type="text" name="content" />
+	<button type="submit" class="btn btn-primary rounded-none w-[300px]">Submit blog post</button>
+</form>
